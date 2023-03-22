61945bcc5922fc4b99f184660f7341fc50986085
Forgottenit <forgottenit@hotmail.com>
2023-03-22 17:44:29 +0000

    Created a model for menu items in menu app and added placeholder styling on index.html

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# insert a menu category model
+# class MenuCategory(models.Model):
+#     name = models.CharField(max_length=20)
+   
+class MenuItems(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    # link to category
+
+    def __str__(self):
+        return self.name
