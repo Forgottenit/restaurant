15925e6d65c633db5d6e8a70f29d5eae64cc7370
Forgottenit <forgottenit@hotmail.com>
2023-03-26 17:00:42 +0000

    Added a Status field to Bookings of pending or confirmed

--- reservations/models.py
+++ reservations/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Reservation(models.Model):
+    PENDING = 'Pending'
+    CONFIRMED = 'Confirmed'
+    # CANCELLED = 'Cancelled'
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (CONFIRMED, 'Confirmed'),
+        # (CANCELLED, 'Cancelled'),
+    ]
     name = models.CharField(max_length=100)
     email = models.EmailField()
     date = models.DateField()
@@ -9,7 +17,7 @@ class Reservation(models.Model):
     special_requests = models.TextField()
     party_size = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(6)])
 
-    # status
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
 
     def __str__(self):
         return f'Bookings are {self.name} {self.email} - {self.date} {self.time} {self.party_size}'
