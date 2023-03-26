0a5687e74e80ec3a0ac2f4b96c5fe209bbe9ff22
Forgottenit <forgottenit@hotmail.com>
2023-03-26 16:25:01 +0000

    Added min/max value on forms party_size so that under 1 and over 6 cant be selected

--- reservations/forms.py
+++ reservations/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Reservation
 
 class BookingForm(forms.ModelForm):
+    party_size = forms.IntegerField(min_value=1, max_value=6)
     class Meta:
         model = Reservation
         fields = ['name', 'email', 'date', 'time', 'special_requests', 'party_size']
