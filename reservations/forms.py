ea3f4a3c703bb8875e81456995a1f04a7bcb73f8
Forgottenit <forgottenit@hotmail.com>
2023-03-26 16:55:13 +0000

    Changed date input to calendar on form

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Reservation
+from django.forms.widgets import DateInput
 
 class BookingForm(forms.ModelForm):
+    YEAR_CHOICES = ['2022', '2023']
+    # MONTH_CHOICES = ['April', 'May', 'June']
     party_size = forms.IntegerField(min_value=1, max_value=6)
+    # date = forms.DateField(widget=forms.SelectDateWidget( years=YEAR_CHOICES))
+    date = forms.DateField(widget=DateInput(attrs={'type': 'date'}))
     class Meta:
         model = Reservation
         fields = ['name', 'email', 'date', 'time', 'special_requests', 'party_size']
