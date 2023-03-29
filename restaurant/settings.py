7c75a335b757cabf6fe940006cc977f54bfa3626
Forgottenit <forgottenit@hotmail.com>
2023-03-29 11:11:52 +0000

    TESTING: created user and made booking and tested redirects and admin

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -157,3 +157,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect login to reservations page
+LOGIN_REDIRECT_URL = 'reservations'
