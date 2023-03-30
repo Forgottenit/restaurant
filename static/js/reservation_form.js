87d377cdafd3890173efbed4a57a8dd78d59eab2
Forgottenit <forgottenit@hotmail.com>
2023-03-30 15:38:22 +0000

    TESTING MODALS

---
+++ static/js/reservation_form.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     // Intercept the form submit event
+//     $('.reservation-form').submit(function (e) {
+//         e.preventDefault();
+//         showModal({
+//             title: 'Confirm Booking',
+//             body: 'Are you sure you want to confirm this booking?',
+//             actionText: 'Confirm Booking',
+//             actionUrl: '#',
+//             actionClass: 'btn-primary'
+//         });
+//     });
+// });
