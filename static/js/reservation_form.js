92c78aef38fb0e845b0321dac50525e3e356e588
Forgottenit <forgottenit@hotmail.com>
2023-04-05 15:21:32 +0000

    Created customModal in html and targeted it on form submission

--- static/js/reservation_form.js
+++ static/js/reservation_form.js
@@ -1,13 +1,37 @@
-// $(document).ready(function () {
-//     // Intercept the form submit event
-//     $('.reservation-form').submit(function (e) {
-//         e.preventDefault();
-//         showModal({
-//             title: 'Confirm Booking',
-//             body: 'Are you sure you want to confirm this booking?',
-//             actionText: 'Confirm Booking',
-//             actionUrl: '#',
-//             actionClass: 'btn-primary'
-//         });
-//     });
-// });
+function showModal(options) {
+    // Set the modal elements based on the options
+    $('#customModalLabel').text(options.title);
+    $('#customModal .modal-body').text(options.body);
+    $('#customModalAction')
+        .text(options.actionText)
+        .addClass(options.actionClass)
+        .off('click')
+        .on('click', function (event) {
+            event.preventDefault();
+            if (options.actionCallback) {
+                options.actionCallback();
+            }
+            $('#customModal').modal('hide');
+        });
+
+    // Display the modal
+    $('#customModal').modal('show');
+}
+
+$(document).ready(function () {
+    // Intercept the form submit event
+    $('.reservation-form').submit(function (e) {
+        e.preventDefault();
+        const form = this;
+
+        showModal({
+            title: 'Confirm Booking',
+            body: 'Are you sure you want to confirm this booking?',
+            actionText: 'Confirm Booking',
+            actionClass: 'btn-primary',
+            actionCallback: function () {
+                form.submit(); // Submit the form
+            }
+        });
+    });
+});
