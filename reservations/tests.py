1c01c3267e1fc94cbda0527010d22e13bbcaacf9
Forgottenit <forgottenit@hotmail.com>
2023-04-14 22:14:41 +0000

    AUTOMATED TESTS: Added automated tests for views.py in restaurant app

--- reservations/tests.py
+++ reservations/tests.py
@@ -1,12 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import User
 from django.utils import timezone
 from datetime import date, time, timedelta
 from .forms import BookingForm
 from .models import Reservation
 from unittest.mock import patch
+from django.urls import reverse
+from .views import reservations, user_reservations, delete_reservation, edit_reservation
 
 
+
+"""
+TESTS FOR FORMS.PY
+"""
 class BookingFormTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
@@ -111,7 +117,6 @@ class BookingFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('party_size', form.errors)
 
-
     def test_within_capacity(self):
         # Test that a form submission matching the restaurant's capacity passes validation
         # Create 9 reservations with a party size of 5, leaving room for one more reservation
@@ -151,7 +156,6 @@ class BookingFormTest(TestCase):
             time='17:00',
             party_size=2,
         )
-        
 
         # Attempt to create another reservation with the same date and different time
         form = BookingForm({
@@ -191,3 +195,75 @@ class BookingFormTest(TestCase):
         self.assertEqual(created_reservation.time.strftime("%H:%M"), form_data['time'])
         self.assertEqual(created_reservation.party_size, form_data['party_size'])
         self.assertEqual(created_reservation.special_requests, form_data['special_requests'])
+
+
+"""
+TESTS FOR VIEWS.PY
+"""
+
+
+class TestReservationsViews(TestCase):
+
+    def setUp(self):
+        # Set up test data and objects for use in tests
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.reservation = Reservation.objects.create(
+            user=self.user,
+            name='John Doe',
+            email='john.doe@example.com',
+            date=timezone.now().date() + timedelta(days=1),
+            time='17:00:00',
+            party_size=2
+        )
+
+    def test_reservations_view_logged_out(self):
+        # Test that logged_out users are redirected (302 STATUS) when trying to accessreservations view
+        response = self.client.get(reverse('reservations'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_reservations_view_logged_in(self):
+        # Test that logged_in users can access the reservations view and that correct template used
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('reservations'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reservations.html')
+        self.assertIsInstance(response.context['form'], BookingForm)
+
+    def test_user_reservations_view_logged_out(self):
+        # Test that logged_out users are redirected (302 STATUS) when trying to access user_reservations view
+        response = self.client.get(reverse('user_reservations'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_reservations_view_logged_in(self):
+        # Test that logged_in users can access the user_reservations view and that correct template used
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('user_reservations'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'successful_booking.html')
+
+    def test_delete_reservation_view_logged_out(self):
+        # Test that logged_out users are redirected when trying to access delete_reservation view
+        response = self.client.get(reverse('delete_reservation', args=[self.reservation.id]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_reservation_view_logged_in(self):
+        # Test that logged_in users can delete a reservation and are redirected to successful_booking 
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('delete_reservation', args=[self.reservation.id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'successful_booking.html')
+        self.assertFalse(Reservation.objects.filter(id=self.reservation.id).exists())
+
+    def test_edit_reservation_view_logged_out(self):
+        # Test that logged_out users are redirected (302 STATUS) when trying to access edit_reservation view
+        response = self.client.get(reverse('edit_reservation', args=[self.reservation.id]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_edit_reservation_view_logged_in(self):
+        # Test that logged_in users can access the edit_reservation view and that correct template is used
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('edit_reservation', args=[self.reservation.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit_reservation.html')
+        self.assertIsInstance(response.context['form'], BookingForm)
