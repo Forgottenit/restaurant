d8ebfacc29e24838ed3e478e403b1d630d7b8006
Forgottenit <forgottenit@hotmail.com>
2023-04-17 20:45:14 +0000

    Added views and styling for staff booking views

--- staff/urls.py
+++ staff/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 
     path('staff_menu/', views.staff_menu, name='staff_menu'),
+    path('all_reservations/', views.all_reservations, name='all_reservations'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -5,6 +5,10 @@ import os
 from django.conf import settings
 from menu.models import MenuItem
 from django.shortcuts import render
+from reservations.models import Reservation
+from datetime import date
+from django.utils import timezone
+from datetime import timedelta
 
 def is_staffteam_or_admin(user):
     return user.groups.filter(name='StaffTeam').exists() or user.is_superuser
@@ -18,10 +22,31 @@ def staff_menu(request):
     return render(request, 'staff_templates/staff_menu.html', context)
 
 
+@user_passes_test(is_staffteam_or_admin)
+def all_reservations(request):
+    past_reservations = Reservation.objects.filter(date__lt=date.today()).order_by('date', 'time')
+    today_reservations = Reservation.objects.filter(date=date.today()).order_by('time')
 
-# @user_passes_test(is_staffteam_or_admin)
-# def staff_menu(request):
-#     items = MenuItem.objects.all()
-#     response = render(request, 'staff_templates/staff_menu.html', {'items': items})
-#     return response
+    today = timezone.now().date()
+    first_day_current_month = today.replace(day=1)
+    future_reservations_by_month = []
 
+    while True:
+        last_day_of_month = (first_day_current_month + timedelta(days=31)).replace(day=1) - timedelta(days=1)
+        reservations = Reservation.objects.filter(date__gte=first_day_current_month, date__lte=last_day_of_month).order_by('date', 'time')
+
+        future_reservations_by_month.append({
+            'title': first_day_current_month.strftime('%B %Y'),
+            'reservations': reservations
+        })
+
+        first_day_current_month = last_day_of_month + timedelta(days=1)
+        if first_day_current_month > today + timedelta(days=365):  # Display reservations for the next 12 months
+            break
+
+    context = {
+        'todays_reservations': today_reservations,
+        'future_reservations_by_month': future_reservations_by_month,
+        'past_reservations': past_reservations,
+    }
+    return render(request, 'staff_templates/all_reservations.html', context)
