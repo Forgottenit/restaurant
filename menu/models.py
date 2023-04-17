310eadc9790b980848554c664d5e2ddae60d6983
Forgottenit <forgottenit@hotmail.com>
2023-04-17 12:23:47 +0000

    Added help text to menu model and defensive code

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class MenuCategory(models.Model):
@@ -15,18 +16,17 @@ class MenuCategory(models.Model):
         (SPECIALS, 'Specials'),
     ]
 
-    name = models.CharField(max_length=20, choices=CATEGORY_CHOICES, unique=True)
+    name = models.CharField(max_length=20, choices=CATEGORY_CHOICES, unique=True, blank=False, null=False, help_text="Select the category")
 
     def __str__(self):
         return self.name
 
 class MenuItem(models.Model):
-    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, default=None)
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, default=None, blank=False, null=False, help_text="Select the category of the menu item")
+    name = models.CharField(max_length=50, blank=False, null=False, help_text="Enter the name of the menu item")
+    description = models.TextField(blank=False, null=False, help_text="Enter a description for the menu item")
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False, validators=[MinValueValidator(0)], help_text="Enter the price of the menu item")
 
     def __str__(self):
         return self.name
-
 
---
+++ static/js/delete_menu_item.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // Show the custom modal when the cancel reservation button is clicked
+    $('.delete-menu-item').click(function (e) {
+        e.preventDefault();
+        let deleteUrl = $(this).data('delete-url');
+
+        showModal({
+            title: 'Delete Menu Item',
+            body: 'Are you sure you want to Delete this Item?',
+            actionText: 'Confirm Deletion',
+            actionClass: 'btn-danger',
+            actionCallback: function () {
+                // Perform the delete action by navigating to the delete URL
+                window.location.href = deleteUrl;
+            }
+        });
+    });
+});
